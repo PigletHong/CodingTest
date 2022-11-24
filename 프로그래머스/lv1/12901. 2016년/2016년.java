7c6cdbef7e430d55aa220863fd64a4f8096dc6b3
PigletHong <116440100+PigletHong@users.noreply.github.com>
2022-11-24 21:47:08 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+        String[] days =  {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+        int[] month = { 31,29,31,30,31,30,31,31,30,31,30,31 };
+        int day = 0;
+        for (int i=0; i<(a-1); i++) {
+            day += month[i];
+        }
+        day += (b-1);
+        day %= 7;
+        answer = days[day];
+        return answer;
+    }
+}
