dd762dc7779213a3dc16adf0672061846f398e48
PigletHong <116440100+PigletHong@users.noreply.github.com>
2023-02-16 21:24:24 +0900

    [level 1] Title: 모의고사, Time: 0.69 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -1,34 +1,29 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 class Solution {
     public int[] solution(int[] answers) {
+        int[] answer = {};
+        int[] person1 = {1,2,3,4,5};
+        int[] person2 = {2,1,2,3,2,4,2,5};
+        int[] person3 = {3,3,1,1,2,2,4,4,5,5};
+        int answer1=0, answer2 =0, answer3 =0;
         
-        int [] first = {1, 2, 3, 4, 5};
-        int [] second = {2, 1, 2, 3, 2, 4, 2, 5};
-        int [] third = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int [] scores = {0,0,0};
-        
-        for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == first[i%5]) scores[0]++;
-            if (answers[i] == second[i%8]) scores[1]++;
-            if (answers[i] == third[i%10]) scores[2]++;
-        }        
-        
-        int max = Math.max(scores[0], Math.max(scores[1], scores[2]));
-
-        List<Integer> numberOne = new ArrayList<Integer>();
-
-        for (int i = 0; i < scores.length; i++) {
-            if (scores[i] == max) {
-                numberOne.add(i+1);
-            }
+        for(int i =0; i<answers.length; i++){
+            if(person1[i%person1.length] == answers[i]) answer1++;
+            if(person2[i%person2.length] == answers[i]) answer2++;
+            if(person3[i%person3.length] == answers[i]) answer3++;
         }
-        int [] answer = new int[numberOne.size()];
-        for (int i = 0; i < numberOne.size(); i++) {
-            answer[i] = numberOne.get(i);
+        int max = Math.max(Math.max(answer1, answer2),answer3);
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        if(max==answer1) list.add(1);
+        if(max==answer2) list.add(2);
+        if(max==answer3) list.add(3);
+        
+        answer = new int[list.size()];
+        
+        for(int i =0; i<answer.length; i++) {
+        	answer[i] = list.get(i);
         }
+        
         return answer;
     }
 }
